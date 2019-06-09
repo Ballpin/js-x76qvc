ffadde5de3724d6cb59de5c20289f99ba0179d08
Ballpin <ivan@it-amigo.se>
2019-06-09 20:02:28 +0200

    2 files modified

--- index.js
+++ index.js
@@ -2,10 +2,31 @@
 import './style.css';
 import { observable, computed, action } from "mobx";
 
-@observable class MyComponent {
+class MyComponent extends HTMLElement {
+  constructir() {
+    super();
+    this.root = this.attachShadow({ mode: 'closed' });
+  }
 
+  @observable person = {
+    firstName: 'Matt',
+    lastName: 'Ruby',
+    age: 37,
+    get fullName() {
+      return this.firstName + ' ' + this.lastName;
+    }
+  };
+
+  connectedCallback() {
+    this.root.innerHTML = `
+      <input id='nameInput' type='text' name='firstName' value='' placeholder='first name'/>
+      <h1>Test</h1>
+    `
+  }
 }
+
+customElements.define('my-comp', MyComponent);
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+appDiv.innerHTML = `<my-comp></my-comp>`;
